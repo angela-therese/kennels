2010290ce2f3da269223c23268166a97ef238fcf
User-42 <a.t.iafrate@gmail.com>
2021-02-25 20:24:57 -0500

    chapter 3, activity 4 nav bar

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css"
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">Luca Iafrate</h3>
+        <div className="customer__address">123 Pokemon Drive</div>
+    </section>
+)
---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,10 @@
+
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="Employee__name">Angela Therese</h3>
+        <div className="Employee__employer">Nashville Kennels North</div>
+    </section>
+)
